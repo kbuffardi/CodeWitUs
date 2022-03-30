21ad0b281e2774bec1684d936fb4f9b039bd50b8
root <ihernandez44@csuchico.edu>
2022-03-30 06:09:22 +0000

    initial tests

--- test/models/interest_test.rb
+++ test/models/interest_test.rb
@@ -1,13 +1,8 @@
 require 'test_helper'
 
 class InterestTest < ActiveSupport::TestCase
-# test "valid attributes should save entry"
-#		interest = Interest.new("id":2, "interest_name":"video games", "interest_count":0)
-#		assert interest.save
-#	end
-#	test "non valid attributes should not save"
-		#params should be int, string, int
-#		interest = Interest.new("id":"12345", "interest_name":123, "interest_count":0)
-#		assert_not interest.save
-#	end
+	test "valid attributes should save entry" do
+		interest = Interest.new("id":2, "interest_name":"video games", "interest_count":0)
+		assert interest.save
+	end
 end
